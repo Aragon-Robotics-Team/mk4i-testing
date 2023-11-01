df7166b37ae5e903e1c92908da73c423fa3b045c
Yoda-Flash <113494066+Yoda-Flash@users.noreply.github.com>
2023-10-31 18:40:33 -0700

    Created skeleton of method
    Imported pathplanner, puzzled things out (somewhat), created part of the follow trajectory command

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -5,10 +5,16 @@
 package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.commands.FollowPathHolonomic;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.PathPlannerTrajectory;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
@@ -55,6 +61,8 @@ public class SwerveDrive extends SubsystemBase {
 
   private final AHRS m_imu = new AHRS();
 
+  private PPHolonomicDriveController m_ppController = new PPHolonomicDriveController(null, null, 5, 0);
+
   public void resetHeading() {
     m_imu.reset();
   }
@@ -77,4 +85,25 @@ public class SwerveDrive extends SubsystemBase {
     m_backLeft.stop();
     m_backRight.stop();
   }
+  // Assuming this method is part of a drivetrain subsystem that provides the necessary methods
+public Command followTrajectoryCommand(PathPlannerTrajectory traj, boolean isFirstPath) {
+  return new SequentialCommandGroup(
+       new InstantCommand(() -> {
+         // Reset odometry for the first path you run during auto
+         if(isFirstPath){
+             this.resetOdometry(traj.getInitialHolonomicPose());
+         }
+       }),
+       new FollowPathHolonomic(
+           traj, 
+           this::getPose, // Pose supplier
+           this.kinematics, // SwerveDriveKinematics
+           new PIDController(0, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+           new PIDController(0, 0, 0), // Y controller (usually the same values as X controller)
+           new PIDController(0, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+           this::setModuleStates, // Module states consumer
+           true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+           this // Requires this drive subsystem
+       )
+   );
 }
